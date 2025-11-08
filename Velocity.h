1be440c085eb9c28596ea511b5d2214c19f6640c
Tomi Nguyen <152843668+tominguyenn@users.noreply.github.com>
2025-11-07 16:07:48 -0800

    updated velocity.h
    Added separate getters for X and Y velocity and acceleration, along with a reset function.

--- Velocity.h
+++ Velocity.h
@@ -8,20 +8,40 @@ class Velocity {
   public:
     Velocity();
     bool begin();
-    void update();        // call in loop()
-    float getVelocity();  // m/s
-    float getYaw();       // degrees
+    void update();
+    
+    // NEW: Separate X and Y velocity getters
+    float getVelocityX();     // Forward/backward velocity
+    float getVelocityY();     // Sideways velocity (for drift!)
+    
+    float getVelocity();      // CHANGED: Now returns total magnitude
+    float getYaw();
+    
+    // NEW: Separate X acceleration getter
+    float getAccelX();
     float getAccelY();
+    
+    // NEW: Reset function
+    void reset();
 
   private:
     MPU6050 mpu;
-    float velocityY;
+    
+    // NEW: Added velocityX (your original only had velocityY)
+    float velocityX;          // Forward/backward velocity
+    float velocityY;          // Sideways velocity
     float yaw;
-    float accY;
+    
+    // NEW: Added accX
+    float accX;               // X acceleration
+    float accY;               // Y acceleration
+    
     unsigned long lastTime;
+    
+    // NEW: Added accX_offset
+    float accX_offset;        // Calibration for X axis
     float accY_offset;
     float gyroZ_offset;
-
 };
 
 #endif
